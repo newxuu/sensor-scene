a2ddbad9ec5b9e75a75edd6762ff6d96350336e6
concefly <concefly@foxmail.com>
2016-12-05 15:48:02 +0800

    stream & classify

---
+++ tools/unpack_mnist.py
@@ -0,0 +1,12 @@
+import cPickle, gzip, numpy
+from scipy.misc import imsave
+import os
+
+f = gzip.open('../dataset/mnist.pkl.gz', 'rb')
+train_set, valid_set = cPickle.load(f)
+f.close()
+
+for i in xrange(100):
+  img = train_set[0][i]
+  label = train_set[1][i]
+  imsave('../dataset/mnist/%s-%s.png' % (i, label), img)
