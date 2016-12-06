605d2ad440eeac89e21183621478721de9fcf8de
concefly <concefly@foxmail.com>
2016-12-06 17:40:58 +0800

    update

--- src/launch.py
+++ src/launch.py
@@ -1,9 +1,5 @@
 # -*- coding:utf-8 -*-
 
-# TODO: logger
-# TODO: clarify the img format. Must be the single channel as th. 
-# TODO: fiting takes too much time
-
 from __future__ import print_function
 
 import os
@@ -11,15 +7,20 @@ import sys
 sys.path.append('lib')
 
 from itertools import izip
+import numpy as np
 import classify
 import stream as img_stream
 import serial
+from log import sys_logger as logger
 
 model = classify.Classify(10)
-imgGenerator = img_stream.camera(0)
+streamFactory = img_stream.Stream()
+imgGenerator = streamFactory.from_camera(0)
 ser = serial.Serial('mock')
 
 scene = 0
+
+logger.info('lanuch successfully')
 
 while True:
 
@@ -29,14 +30,17 @@ while True:
     # fit the model
     if cmd == 0:
       sid = payload
+      logger.info('fitting sid = %d', sid)
       # fit 3 times
       for _, item in izip(xrange(3), imgGenerator):
         img, stamp = item
-        model.fit_once(img, sid)
+        model.fit_single(img, sid)
 
   # check scene
-  tmp = model.predict(imgGenerator.next())
-  if not tmp == scene:
+  img, stamp = imgGenerator.next()
+  sid, sid_p = model.predict(img)
+  logger.debug('predict sid = %s with %s chance', sid, sid_p)
+  if not sid == scene:
     # feedback the scene id
-    scene = tmp
+    scene = sid
     ser.send(1, scene)
