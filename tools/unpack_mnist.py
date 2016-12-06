421d8f669797bfff51b35a4329fd4d7653660939
concefly <concefly@foxmail.com>
2016-12-06 17:57:10 +0800

    more mnist imgs

--- tools/unpack_mnist.py
+++ tools/unpack_mnist.py
@@ -6,7 +6,7 @@ f = gzip.open('../dataset/mnist.pkl.gz', 'rb')
 train_set, valid_set = cPickle.load(f)
 f.close()
 
-for i in xrange(100):
+for i in xrange(1000):
   img = train_set[0][i]
   label = train_set[1][i]
   imsave('../dataset/mnist/%s-%s.png' % (i, label), img)
